9437ab3c7b72b219e46d6da1697f9325a2d1208b
Marialafu <lafuentelhmaria@gmail.com>
2025-05-22 18:28:22 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Button from './components/button/Button';
+import { GlobalStyles } from './styles/GlobalStyles';
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <Button />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/button/Button.jsx
@@ -0,0 +1,7 @@
+import { StyledButton } from './buttonStyles';
+
+const Button = () => {
+  return <StyledButton>UN VIERNES MUY LUNES</StyledButton>;
+};
+
+export default Button;
